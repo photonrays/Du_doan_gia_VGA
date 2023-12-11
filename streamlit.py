d55441cab28ed5ef8a9d495f8718c7fb9848ffcb
photonrays <lthang052@gmail.com>
2023-12-11 17:08:35 +0700

    clean data and dashboard

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("VGA")
+
+st.set_page_config(
+    page_title="VGA Report",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+st.title("VGA Report")
+
 
 @st.cache_data
 def load_data(path: str):
@@ -21,24 +35,19 @@ df = load_data(uploaded_file)
 with st.expander("Data Preview"):
     st.dataframe(df)
 
-brands = ['NVIDIA', 'MSI', 'Intel']
 
-def plot_left_most(feature):
-    fig, ax = plt.subplots()
-    sns.regplot(x=feature, y='Price', data=df, ax=ax)
-    ax.set_title(f'Regplot of {feature} vs Price')
-    st.pyplot(fig)
-
-def plot_boxplot_and_pie_chart(selected_feature):
+def plot_categorical_feature(selected_feature):
     fig, axes = plt.subplots(1, 2, figsize=(15, 5))
 
-    # Plot pie chart
+    # Plot pie chart with all values
     ax_pie = axes[0]
     feature_counts = df[selected_feature].value_counts()
-    filtered_counts = feature_counts[feature_counts / feature_counts.sum() >= 0.05]
-    other_count = feature_counts[feature_counts / feature_counts.sum() < 0.05].sum()
-    filtered_counts['Other'] = other_count
-    ax_pie.pie(filtered_counts, labels=filtered_counts.index, autopct='%1.1f%%', startangle=90)
+    wedges, _, _ = ax_pie.pie(feature_counts, labels=None, autopct='', startangle=90)
+
+    # Add legend showing the pair of color, label, and percentage
+    labels = [f'{label} ({percentage:.1f}%)' for label, percentage in zip(feature_counts.index, feature_counts / feature_counts.sum() * 100)]
+    ax_pie.legend(wedges, labels, title="Legend", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+
     ax_pie.axis('equal')
     ax_pie.set_title(f'Pie Chart of {selected_feature}')
 
@@ -51,24 +60,25 @@ def plot_boxplot_and_pie_chart(selected_feature):
     plt.tight_layout()
     return fig
 
-def plot_left_most(selected_feature):
+
+def plot_numerical_features(selected_feature):
     fig, axes = plt.subplots(1, 2, figsize=(15, 5))
 
     sns.regplot(x=selected_feature, y='Price', data=df, ax=axes[0])
     axes[0].set_title(f'Regplot of {selected_feature} vs Price')
 
     sns.lineplot(x=selected_feature, y='Price', data=df, ax=axes[1]) 
-    axes[1].set_title(f'Line Plot of AnotherNumericFeature vs Price')
+    axes[1].set_title(f'Line Plot of {selected_feature} vs Price')
     plt.tight_layout()
 
     return fig
 
 st.header("Correlation of Numeral features")
 selected_feature = st.selectbox("Select a numerical feature:", [col for col in df.columns if pd.api.types.is_numeric_dtype(df[col]) and col != 'Price'])
-fig_numerical = plot_left_most(selected_feature)
+fig_numerical = plot_numerical_features(selected_feature)
 st.pyplot(fig_numerical)
 
 st.header("Correlation of Categorical features:")
 selected_feature = st.selectbox("Select a categorical feature:", [col for col in df.columns if pd.api.types.is_object_dtype(df[col]) and col != 'Name'])
-fig_boxplot = plot_boxplot_and_pie_chart(selected_feature)
+fig_boxplot = plot_categorical_feature(selected_feature)
 st.pyplot(fig_boxplot)
